93b2e8d1a6c1519a9852d1a729ba4354ee1baa02
= <=>
2024-06-11 08:32:27 +0800

    filter neon origins

--- dist/background.js
+++ dist/background.js
@@ -3,19 +3,21 @@ let capturedRequests = [];
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    if (details.method === "POST" && details.requestBody) {
-      let payload;
-      if (details.requestBody.formData) {
-        payload = details.requestBody.formData;
-      } else if (details.requestBody.raw) {
-        const rawBytes = details.requestBody.raw[0].bytes;
-        payload = new TextDecoder().decode(rawBytes);
+    if(!details.url.includes("neonaiserver.onrender.com") && !details.url.includes("neonremote.netlify.app")){
+      if (details.method === "POST" && details.requestBody) {
+        let payload;
+        if (details.requestBody.formData) {
+          payload = details.requestBody.formData;
+        } else if (details.requestBody.raw) {
+          const rawBytes = details.requestBody.raw[0].bytes;
+          payload = new TextDecoder().decode(rawBytes);
+        }
+        console.log('Request payload:', payload);
+        capturedRequests.push({ url: details.url, payload: payload, method: details.method });
       }
-      console.log('Request payload:', payload);
-      capturedRequests.push({ url: details.url, payload: payload, method: details.method });
-    }
-    else{
-      capturedRequests.push({ url: details.url, payload: "N/A", method: details.method });
+      else{
+        capturedRequests.push({ url: details.url, payload: "N/A", method: details.method });
+      }
     }
   },
   { urls: ["<all_urls>"] },
